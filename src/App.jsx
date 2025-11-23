dfb142b61702cb09a8392996771732c0b37c2e56
MiguelPachon <miguel.pachon@estudiantesunibague.edu.co>
2025-11-23 14:11:24 -0500

    renirigir al home con google

--- src/App.jsx
+++ src/App.jsx
@@ -200,6 +200,7 @@ function App() {
       addNotification("Sesión iniciada correctamente ✅");
 
       setShowLoginModal(false);
+      setCurrentView("home");
       setLoginForm({ email: "", password: "" });
       setLoginErrors({});
     } catch (error) {
